209cf85bd9b9ca5d97ea1d26a3c83afbd33f90d2
Chris Simpson <c.a.simpson01@gmail.com>
2016-04-13 10:16:46 +0100

    Split out peak fit functionality.

--- pyxe/area_analysis.py
+++ pyxe/area_analysis.py
@@ -10,6 +10,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from six import string_types
 import unittest
 import os
 import fabio
@@ -22,8 +23,6 @@ from pyxe.fitting_tools import array_fit
 from pyxe.fitting_functions import cos_
 from pyxe.strain_tools import StrainTools
 from pyxe.plotting import StrainPlotting
-
-
 
 
 def exclusion(file_list, exclusion_list):
@@ -58,110 +57,109 @@ class Area(StrainTools, StrainPlotting):
     associated error. 
     """
    
-    def __init__(self, folder, pos_data, det_params, q0, window, f_ext = '.edf', 
-                 mirror = True, func = 'gaussian', error_limit = 2 * 10 ** -4, output = 'simple',
-                 pos_file_delimiter = ',', exclude = [], npt_rad = 1024, npt_azim = 36, 
-                 azimuth_range = [-180, 180]):
+    def __init__(self, folder, pos_data, det_params, f_ext = '.edf', 
+                 pos_delim = ',', exclude = [], npt_rad = 1024, npt_az = 36, 
+                 az_range = [-180, 180]):
         """
-        
+        # folder:     Folder containing the image files for analysis
+        # pos_data:   Either csv file or numpy array containing position data 
         # det_params: Accepts a file location for a .poni parameter file 
                       produced by pyFAI-calib. Alternative directly enter 
                       calibration details as take from Fit2D in form:
                       (sample_to_detector (mm), centre_x (pix), centre_y (pix), 
                       tilt (deg), tilt_plane (deg), pixel_size_x (micron), 
                       pixel_size_y (micron), wavelength (m))
-                      
-        # azim_range: Range of azimuthal values to investigate - note that 0
+        # pos_delim:  If in .csv format the file delimiter              
+        # az_range:   Range of azimuthal values to investigate - note that 0
                       degrees is defined at the eastern edge of the circle.
         """
         error = 'Azimuthal range must be less than or equal to 360deg'
-        assert abs(np.max(azimuth_range) - np.min(azimuth_range)) <= 360, error
+        assert abs(np.max(az_range) - np.min(az_range)) <= 360, error
         if isinstance(det_params, ("".__class__, u"".__class__)):
             ai = pyFAI.load(det_params) ## CORRECT??
-            
         else:
             ai = pyFAI.AzimuthalIntegrator()
             ai.setFit2D(*det_params[:-1])
             ai.set_wavelength(det_params[-1])
         print(ai)
         
-        
-        #exclude.append(os.path.split(pos_data)[1]) # Now search for actual files...
-        fnames = sorted([x for x in os.listdir(folder) if x[-len(f_ext):] == f_ext])
-        
-        print(len(fnames))
+        fnames = sorted([x for x in os.listdir(folder) if x.endswith(f_ext)])
         fnames = exclusion(fnames, exclude)
-        print(fnames)
         error = ('Position data not supplied in correct format: \n'
                  ' pos_data variable must be the (str) path to a csv file, '
                  'a numpy array containing 1, 2 or 3d co-ordinates')
-        assert isinstance(pos_data, ("".__class__, u"".__class__, np.ndarray, tuple)), error
-        if isinstance(pos_data, ("".__class__, u"".__class__)):
-            positions = np.loadtxt(pos_data, delimiter = pos_file_delimiter)
+        assert isinstance(pos_data, (string_types, np.ndarray, tuple)), error
+        if isinstance(pos_data, string_types):
+            positions = np.loadtxt(pos_data, delimiter = pos_delim)
         else:
             positions = pos_data
         
-        print(fnames, positions)
-        error = 'Number of positions not equal to number of files (pos = %s,' \
-                ' files = %s)' % (positions.shape[0], len(fnames))
+        error = ('Number of positions not equal to number of files (pos = %s,' 
+                ' files = %s)' % (positions.shape[0], len(fnames)))
         assert positions.shape[0] == len(fnames), error
 
         (self.ss2_x, self.ss2_y, self.ss2_z), self.dims = dim_fill(positions)   
         self.co_ords = {b'ss2_x': self.ss2_x, b'ss2_y': self.ss2_y, 
                         b'self_z': self.ss2_z} 
-        
-        # Convert int or float to list
-        self.q0 = [q0] if isinstance(q0, (int, float, np.float64)) else q0
-        self.peak_windows = [[q_ - window/2, q_ + window/2] for q_ in self.q0]
-        
-        # Accept detector specific q0 2d-array
-        if len(np.shape(self.q0)) == 2:
-            q0_av = np.nanmean(self.q0, 0)
-            self.peak_windows = [[q_ - window/2, q_ +window/2] for q_ in q0_av]
-        
-        array_shape = (positions.shape[0], npt_azim, ) + (np.shape(self.q0)[-1],)
-        data = [np.nan * np.ones(array_shape) for i in range(4)]
-        self.peaks, self.peaks_err, self.fwhm, self.fwhm_err = data
-
-        # consider saving all profiles?        
-        shape_I = (positions.shape[0], npt_azim, npt_rad)
+              
+        shape_I = (positions.shape[0], npt_az, npt_rad)
         self.I = np.nan * np.ones((shape_I))
         for fidx, fname in enumerate(fnames):
             img = fabio.open(os.path.join(folder, fname)).data
-            I, q_, phi = ai.integrate2d(img, npt_rad = npt_rad, npt_azim = npt_azim, azimuth_range = azimuth_range, unit='q_A^-1')
+            I, q_, phi = ai.integrate2d(img, npt_rad=npt_rad, npt_azim=npt_az, 
+                                        azimuth_range=az_range, unit='q_A^-1')
             self.I[fidx] = I         
             
-            q = np.repeat(q_[None, :], npt_azim, axis = 0)
-
+        self.q = np.repeat(q_[None, :], npt_az, axis = 0)
+        self.phi = phi * np.pi / 180  
+        
+        
+        def peak_fit(self, q0, window, mirror = True, func = 'gaussian', 
+                     error_limit = 2 * 10 ** -4, output = 'simple'):
+            
+            # Convert int or float to list
+            self.q0 = [q0] if isinstance(q0, (int, float, np.float64)) else q0
+            self.peak_windows = [[q_ - window/2, q_ + window/2] for q_ in self.q0]
+            
+            # Accept detector specific q0 2d-array
+            if len(np.shape(self.q0)) == 2:
+                q0_av = np.nanmean(self.q0, 0)
+                self.peak_windows = [[q_ - window/2, q_ +window/2] for q_ in q0_av]
+            
+            array_shape = (positions.shape[0], npt_az, ) + (np.shape(self.q0)[-1],)
+            data = [np.nan * np.ones(array_shape) for i in range(4)]
+            self.peaks, self.peaks_err, self.fwhm, self.fwhm_err = data
+            
             for idx, window in enumerate(self.peak_windows):
-                a, b, c, d = array_fit(q, I, window, func, error_limit, output, unused_detectors = [])
+                a, b, c, d = array_fit(self.q, I, window, func, error_limit, output, unused_detectors = [])
                 self.peaks[fidx, ..., idx], self.peaks_err[fidx, ..., idx] = a, b
                 self.fwhm[fidx, ..., idx], self.fwhm_err[fidx, ..., idx] = c, d
-        self.q = q
-        print(phi)        
-        self.phi = phi * np.pi / 180        
-        
-        if mirror:
-            # has to be even number of slices but uneven number of boundaries.
-            angles = self.phi[:int(self.phi[:].shape[0]/2)]
-            #discard = self.phi[int(self.phi[:].shape[0]/2):]
-            peak_shape = self.peaks.shape
-            phi_len = int(peak_shape[-2]/2)
-            new_shape = (peak_shape[:-2] + (phi_len, ) + peak_shape[-1:]) 
-            print(new_shape)
-            mirror_peak = np.nan * np.zeros(new_shape)
-            mirror_err = np.nan * np.zeros(new_shape)
-            for i in range(phi_len):
-                mirror_peak[:, i] = (self.peaks[:, i] + self.peaks[:, i + new_shape[-2]]) / 2
-                mirror_err[:, i] = (self.peaks_err[:, i] + self.peaks_err[:, i + new_shape[-2]]) / 2
-            self.peaks = mirror_peak
-            self.phi = angles
-            self.peaks_err = mirror_err
+       
             
-        self.slit_size = []
-        self.strain = (self.q0 - self.peaks)/ self.q0
-        self.strain_err = (self.q0 - self.peaks_err)/ self.q0
-        self.strain_fit(error_limit)
+            if mirror:
+                # has to be even number of slices but uneven number of boundaries.
+                angles = self.phi[:int(self.phi[:].shape[0]/2)]
+                peak_shape = self.peaks.shape
+                phi_len = int(peak_shape[-2]/2)
+                new_shape = (peak_shape[:-2] + (phi_len, ) + peak_shape[-1:]) 
+                empty_arrays = [np.nan * np.zeros(new_shape) for i in range(4)]
+                mirror_peak, mirror_err, mirror_fwhm, mirror_fwhm_err = empty_arrays
+
+                for i in range(phi_len):
+                    mirror_peak[:, i] = (self.peaks[:, i] + self.peaks[:, i + new_shape[-2]]) / 2
+                    mirror_err[:, i] = (self.peaks_err[:, i] + self.peaks_err[:, i + new_shape[-2]]) / 2
+                    mirror_fwhm[:, i] = (self.fwhm[:, i] + self.fwhm[:, i + new_shape[-2]]) / 2
+                    mirror_fwhm_err[:, i] = (self.fwhm_err[:, i] + self.fwhm_err[:, i + new_shape[-2]]) / 2
+                self.peaks = mirror_peak
+                self.phi = angles
+                self.peaks_err = mirror_err
+                self.fwhm = mirror_fwhm
+                self.fwhm_err = mirror_fwhm_err
+                
+            self.slit_size = []
+            self.strain = (self.q0 - self.peaks)/ self.q0
+            self.strain_err = (self.q0 - self.peaks_err)/ self.q0
+            self.strain_fit(error_limit)
 
 
 
