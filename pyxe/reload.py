dbdc7ec150664f6968ba1dad1afccb9beb7b72ac
Chris Simpson <c.a.simpson01@gmail.com>
2016-04-13 15:51:00 +0100

    Removed function mirrored in helper files.

--- pyxe/reload.py
+++ pyxe/reload.py
@@ -13,24 +13,8 @@ from __future__ import unicode_literals
 
 from pyxe.plotting import StrainPlotting
 from pyxe.strain_tools import StrainTools
+from pyxe.analysis_tools import dimension_fill
 import h5py
-
-
-
-def dimension_fill(data, dim_ID):
-    """
-    Extracts correct spatial array from hdf5 file. Returns None is the
-    dimension doesn't exist.
-    
-    # data:       Raw data (hdf5 format)   
-    # dim_ID:     Dimension ID (ss_x, ss2_y or ss2_z)
-    """
-    try:
-        dimension_data = data['entry1/EDXD_elements/' + dim_ID][:]
-    except KeyError:
-        dimension_data = None
-    return dimension_data
-
 
 
 class Reload(StrainTools, StrainPlotting):
@@ -41,42 +25,38 @@ class Reload(StrainTools, StrainPlotting):
     """
     def __init__(self, file):
 
-        self.filename = file
+        self.name = file
         self.f = h5py.File(file, 'r') 
-        group = self.f['entry1']['EDXD_elements']
+        group = self.f['entry1/EDXD_elements']
         
+        # 'raw' data
+        self.dims = list(group['dims'][:])
+        self.phi = group['phi'][:]
+        self.I = group['data'][:]
+        self.q = group['q'][:] 
+        self.ss2_x = dimension_fill(self.f, 'ss2_x')   
+        self.ss2_y = dimension_fill(self.f, 'ss2_y')
+        self.ss2_z = dimension_fill(self.f, 'ss2_z')
+        self.co_ords = {b'ss2_x': self.ss2_x,b'ss2_y': self.ss2_y, 
+                        b'self_z': self.ss2_z} 
+        # 'calculated' data
         try:        
             self.strain = group['strain'][:]
         except KeyError as e:
             e.args += ('Invalid .nxs file - no strain data found.',
                        'Run XRD_analysis tool.')
             raise
-
-        self.dims = list(group['dims'][:])
-        self.phi = group['phi'][:]
         self.q0 = group['q0'][:]
         self.peak_windows = group['peak_windows'][:]
         self.peaks = group['peaks'][:]
         self.peaks_err = group['peaks_err'][:]    
-    #try:
         self.fwhm = group['fwhm'][:]
         self.fwhm_err = group['fwhm_err'][:]
-    #except:
-        #pass
         self.strain_err = group['strain_err'][:]
         self.strain_param = group['strain_param'][:]
         
-        self.q = group['q'][:] 
-        self.I = group['data'][:]
         
-        self.ss2_x = dimension_fill(self.f, 'ss2_x')   
-        self.ss2_y = dimension_fill(self.f, 'ss2_y')
-        self.ss2_z = dimension_fill(self.f, 'ss2_z')
-        self.co_ords = {b'ss2_x': self.ss2_x,b'ss2_y': self.ss2_y, 
-                        b'self_z': self.ss2_z} 
-        
-  
-    def save_to_nxs(self, fname):
+    def save_to_nxs(self, fname=None):
         """
         Saves all data back into an expanded .nxs file. Contains all original 
         data plus q0, peak locations and strain.
@@ -84,7 +64,8 @@ class Reload(StrainTools, StrainPlotting):
         # fname:      File name/location - default is to save to parent 
                       directory (*_md.nxs) 
         """
-        fname = fname + '_md.nxs'
+        if fname == None:        
+            fname = '%s_md.nxs' % self.name[:-4]
         
         with h5py.File(fname, 'w') as f:
             data_ids = ('dims', 'phi', 'q0','peak_windows', 
@@ -103,6 +84,4 @@ class Reload(StrainTools, StrainPlotting):
                     f.create_dataset(base_tree % data_id, data = data, compression = 'gzip')
                 else:
                     f.create_dataset(base_tree % data_id, data = data)
-
-
 
