433806acd71195c0205776ed6708047aff6c575d
Chris Simpson <c.a.simpson01@gmail.com>
2016-04-15 14:38:18 +0100

    Updated version.

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
 
 setup(
     name='pyxe',
-    version='0.7.1',
+    version='0.8.0',
     author='C. Simpson',
     author_email='c.a.simpson01@gmail.com',
     packages=['pyxe'],
     url='https://github.com/casimp/pyxe',
-    download_url = 'https://github.com/casimp/pyxe/tarball/v0.7.1',
+    download_url = 'https://github.com/casimp/pyxe/tarball/v0.8.0',
     license='LICENSE.txt',
     description='XRD strain analysis package. Efficient analysis and visulisation of diffraction data.',
     keywords = ['XRD', 'EDXD', 'x-ray', 'diffraction', 'strain', 'synchrotron'],
