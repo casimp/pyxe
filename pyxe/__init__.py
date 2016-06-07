2a07064572dc2c920352a926d3fc8eb603d3183f
Chris Simpson <c.a.simpson01@gmail.com>
2016-06-07 22:33:23 +0100

    UPdated __init__.py.

--- pyxe/__init__.py
+++ pyxe/__init__.py
@@ -5,6 +5,6 @@ Created on Mon Nov 30 13:38:59 2015
 @author: casimp
 """
 
-from pyxe.edi12_analysis import EDI12
-from pyxe.merge import Merge
-from pyxe.strain_tools import StrainTools
+#from pyxe.edi12_analysis import EDI12
+#from pyxe.merge import Merge
+#from pyxe.strain_tools import StrainTools
