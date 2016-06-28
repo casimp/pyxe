6c57568528168acae73c4da7c9c41fb5ab544e42
Chris Simpson <c.a.simpson01@gmail.com>
2016-06-28 07:49:49 +0100

    Split out full_ring_fitting and saving functionality.

--- pyxe/analysis_tools.py
+++ pyxe/analysis_tools.py
@@ -13,6 +13,69 @@ from __future__ import unicode_literals
 import numpy as np
 
 
+def full_ring_fit(strain, phi):
+    """
+    Fits the strain transformation equation to the strain information from each
+    azimuthal slice.
+    """
+    strain_tensor = np.nan * np.ones(strain.shape[:-1] + (3,))
+
+    error_count = 0
+    for idx in np.ndindex(strain.shape[:-1]):
+        data = strain[idx]
+        not_nan = ~np.isnan(data)
+
+        if phi[not_nan].size > 2: # Nope use sampling theory to define minimum
+            # Estimate curve parameters
+            p0 = [np.nanmean(data), 3 * np.nanstd(data) / (2 ** 0.5), 0]
+            try:
+                a, b = curve_fit(strain_transformation,
+                                 phi[not_nan], data[not_nan], p0)
+                strain_tensor[idx] = a
+            except (TypeError, RuntimeError):
+                error_count += 1
+        else:
+            error_count += 1
+    print('\nUnable to fit full ring (%s) data %i out of %i points'
+          % (name, error_count, np.size(strain[..., 0])))
+
+    return strain_tensor
+
+
+def pyxe_to_nxs(fname, pyxe_object, overwrite=False):
+    """
+    Saves all data back into an expanded .nxs file. Contains all original
+    data plus q0, peak locations and strain.
+
+    # fname:      File name/location - default is to save to parent
+                  directory (*_pyxe.nxs)
+    """
+    data_ids = ['d1', 'd2', 'd3', 'phi', 'q', 'I', 'n_dims']
+    data_array = [pyxe_object.d1, pyxe_object.d2, pyxe_object.d3,
+                  pyxe_object.phi, pyxe_object.q, pyxe_object.I,
+                  pyxe_object.n_dims]
+
+    if pyxe_object.analysis_stage > 0:
+        data_ids += ['peaks', 'peaks_err', 'fwhm', 'fwhm_err']
+        data_array += [pyxe_object.peaks, pyxe_object.peaks_err,
+                       pyxe_object.fwhm, pyxe_object.fwhm_err]
+    if pyxe_object.analysis_stage == 2:
+        data_ids += ['strain', 'strain_err', 'strain_tensor', 'q0']
+        data_array += [pyxe_object.strain, pyxe_object.strain_err,
+                       pyxe_object.strain_tensor]
+
+    write = 'w' if overwrite else 'w-'
+    with h5py.File(fname, write) as f:
+
+        for data_id, data in zip(data_ids, data_array):
+            d_path = 'entry1/pyxe_simplified/%s' % data_id
+
+            if data_id == 'I':
+                f.create_dataset(d_path, data=data, compression='gzip')
+            else:
+                f.create_dataset(d_path, data=data)
+
+
 def dim_fill(data):
 
     co_ords = []
@@ -27,7 +90,7 @@ def dim_fill(data):
         except IndexError:
             co_ords.append(None)
     return co_ords, dims
-    
+
 
 def mirror_data(phi, data):
     # has to be even number of slices but uneven number of boundaries.
